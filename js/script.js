a477558414354eb0710003af0eea2313c26ea127
Danil <rdix3@yandex.ru>
2021-12-26 14:58:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,216 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Tabs
+  let   tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show');
+    });
+
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    }); 
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.remove('hide');
+    tabsContent[i].classList.add('show');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+  
+  // Timer
+
+  const deadLine = '2022-01-01';
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+          days = Math.floor(t / (1000 * 60 * 60 * 24)),
+          hours = Math.floor(Math.floor((t / (1000 * 60 * 60)) % 24)),
+          minutes = Math.floor((t / (1000 * 60) % 60)),
+          seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+    
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);  
+      }
+    }
+  }
+
+  setClock('.timer', deadLine);
+
+  // Modal
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modalContent = document.querySelector('.modal'),
+        modalClose = document.querySelector('[data-close]');
+
+  function openModal() {
+    modalContent.classList.add('show');
+    modalContent.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+  }
+
+  function closeModal() {
+    modalContent.classList.add('hide');
+    modalContent.classList.remove('show');
+    document.body.style.overflow = '';
+  }
+  
+  modalTrigger.forEach(item => {
+    item.addEventListener('click', () => {
+      openModal();
+    });
+  });
+  
+  modalClose.addEventListener('click', closeModal);
+
+  modalContent.addEventListener('click', (e) => {
+    if (e.target === modalContent) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape' && modalContent.classList.contains('show')) {
+      closeModal();
+    }
+  });
+
+  const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
+
+
+  // Classes
+
+  class FoodCard {
+    constructor(src, alt, title, description, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.description = description;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.changeToUAH();
+    }
+  
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+  
+    render() {
+      const element = document.createElement('div');
+      element.innerHTML = `
+        <div class="menu__item">
+                        <img src=${this.src}>
+                        <h3 class="menu__item-subtitle">${this.title}</h3>
+                        <div class="menu__item-descr">${this.description}</div>
+                        <div class="menu__item-divider"></div>
+                        <div class="menu__item-price">
+                            <div class="menu__item-cost">Цена:</div>
+                            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                        </div>
+                    </div>
+      `;
+      this.parent.append(element);
+    }
+  
+  }
+
+  new FoodCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    ".menu .container"
+  ).render();
+
+  new FoodCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    'Меню "Премиум"',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    14,
+    ".menu .container"
+  ).render();
+
+  new FoodCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    21,
+    ".menu .container"
+  ).render();
+
+  
+
+
+
+});
